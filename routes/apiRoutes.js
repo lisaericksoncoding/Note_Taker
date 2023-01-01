1c09f3ee10a88ddad39360c6ca5b87d25a01ce6e
lisaericksoncoding <111710705+lisaericksoncoding@users.noreply.github.com>
2023-01-01 17:51:09 -0600

    api route

---
+++ routes/apiRoutes.js
@@ -0,0 +1,43 @@
+const api = require('express').Router();
+const path = require('path');
+const fs = require('fs');
+// Helper method for generating unique ids
+const uuid = require('./helpers/uuid');
+
+api.post('/notes', (req, res) => {
+    let existingNotes = JSON.parse(fs.readFileSync(path.join(__dirname, '../db/db.json')));
+
+    const { title, text } = req.body;
+
+    const newNote = {
+        title, 
+        text,
+        id: uuid()
+    }
+existingNotes.push(newNote);
+fs.writeFileSync(path.join(__dirname, '../db/db.json'), JSON.stringify(existingNotes));
+res.json(existingNotes);
+}) 
+
+api.get('/notes', (req, res) => {
+    let existingNotes = JSON.parse(fs.readFileSync(path.join(__dirname, '../db/db.json')));
+    res.json(existingNotes);
+})
+
+api.delete('/notes', (req, res) => {
+    let existingNotes = JSON.parse(fs.readFileSync(path.join(__dirname, '../db/db.json')));
+    for (let i = 0; i < existingNotes.length; i++) {
+        if (existingNotes[i].id == req.params.id) {
+            existingNotes.splice(i, 1);
+            break;
+        }
+    }
+    fs.writeFileSync(path.join(__dirname, '../db/db.json'), JSON.stringify(existingNotes), function (err){
+        if (err) {
+            return console.log(err);
+        } else {
+            console.log("Your note has been successfully deleted.")
+        }
+    });
+    res.json(existingNotes);
+})
