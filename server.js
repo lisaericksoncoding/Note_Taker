db19f68f54232ed7029c17c99525586862b1d511
lisaericksoncoding <111710705+lisaericksoncoding@users.noreply.github.com>
2023-01-07 13:46:53 -0600

    update to port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
